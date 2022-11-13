6ede429762b9a10ca222de5908f6a8714a5824a5
Atiar Rahman Nayeem <atiar.rahman@gigatechltd.com>
2022-11-13 17:21:05 +0600

    updated employee section

---
+++ src/app/modules/features/models/employee.model.ts
@@ -0,0 +1,16 @@
+export interface Employee {
+    id: number;
+    employeeCode: string;
+    email: string;
+    name: string;
+    mobileNumber: string;
+    designation: string;
+    payrollDesignation: string;
+    supervisorId: string;
+    bloodGroup: string;
+    dob: string;
+    extension: string;
+    officeName: string;
+    officeAddress: string;
+    sittingLocation: string;
+}
