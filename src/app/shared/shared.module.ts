09dc8cb708f25c779f6de86dafeb1a6e9fe52aa7
Atiar Rahman Nayeem <atiar.rahman@gigatechltd.com>
2022-11-23 11:44:11 +0600

    dark mode updated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,22 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DarkModeToggleComponent } from './components/dark-mode-toggle/dark-mode-toggle.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatIconModule],
-    exports: [CommonModule, FormsModule, ReactiveFormsModule, DarkModeToggleComponent],
-    declarations: [
-    DarkModeToggleComponent
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        MatTooltipModule,
+    ],
+    exports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        DarkModeToggleComponent,
+    ],
+    declarations: [DarkModeToggleComponent],
 })
 export class SharedModule {}
