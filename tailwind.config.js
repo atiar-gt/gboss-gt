5d41919ccf6be849a07d39c8ecd111e327e34b27
Atiar Rahman Nayeem <atiar.rahman@gigatechltd.com>
2022-12-01 17:12:42 +0600

    dashboard updating

--- tailwind.config.js
+++ tailwind.config.js
@@ -88,7 +88,7 @@ const config = {
         screens : {
             sm: '600px',
             md: '960px',
-            lg: '1280px',
+            lg: '1650px',
             xl: '1440px'
         },
         extend  : {
